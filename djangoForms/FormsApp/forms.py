81f70937a0738d3c51b63fd298f36c3d2eaafa97
8912Soundariya <soundariya_12@gmail.com>
2021-06-24 07:22:47 +0000

    adding

--- djangoForms/FormsApp/forms.py
+++ djangoForms/FormsApp/forms.py
@@ -1,7 +1,7 @@
 from django import forms
 from .models import Student
 
-Model Form
+#Model Form
 class studentRegistration(forms.ModelForm):
     class Meta:
         model = Student
@@ -10,7 +10,7 @@ class studentRegistration(forms.ModelForm):
         exclude = ['contact']
         
 # Django Form without model        
-class EmployeeRegistration():
+class EmployeeRegistration(forms.Form):
     firstname = forms.CharField(label="Enter first name",max_length=50)  
     lastname  = forms.CharField(label="Enter last name", max_length = 100)
     Email     = forms.EmailField(label="Enter the valid email id", max_length=50)
