8fea8286b130dbbf1b1205bc23f1dd033a0ccf85
8912Soundariya <soundariya_12@gmail.com>
2021-06-24 12:36:38 +0000

    Django validation form

--- djangoForms/FormsApp/views.py
+++ djangoForms/FormsApp/views.py
@@ -8,6 +8,21 @@ def showFormData(request):
     return render(request, 'register/stuRegistration.html', {'form': fm})
     
 def showEmpForm(request):
-    fm = EmployeeRegistration()
+    if request.method == 'POST':
+        fm = EmployeeRegistration(request.POST)
+        if fm.is_valid():
+            Firstname = fm.cleaned_data['firstname']
+            lastname = fm.cleaned_data['lastname']
+            Email = fm.cleaned_data['Email']
+            Indian =  fm.cleaned_data['Indian']
+            Mobile = fm.cleaned_data['Mobile']
+            
+            print('Name: ', Firstname + lastname)
+            print('Email Id: ', Email)
+            print('Are you indian? : ', Indian)
+            print('Contact No.', Mobile)
+    else:
+        fm = EmployeeRegistration()
+        print('Hitted the GET request')
     return render(request, 'register/empRegistration.html', {'form': fm})
     
